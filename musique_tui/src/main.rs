6fcfebb87a07c6128ce2fb1ed62f1ef12573334e
Woudg12 <timurka.istanbul@gmail.com>
2026-04-26 21:14:30 +0300

    skibidi AHHHH

--- musique_tui/src/main.rs
+++ musique_tui/src/main.rs
@@ -3,4 +3,13 @@ use crossterm::event::{self, eventi KeyCode};
 use std::fs::OpenOptions;
 use std::io::Write;
 
-const PİPE: &str = "/tmp"
+const PİPE: &str = "/tmp/musique_cmd"
+
+fn send(cmd: &str ) {
+    if let ok(mut f) = OpenOptions::new().write(true).open(PİPE) {
+        let _ = writeln!(f, "{}", cmd);
+
+    }
+}
+
+fn main
