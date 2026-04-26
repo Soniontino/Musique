f9689b1e427ce2bdc26bba31a67e1627a8c61bae
Woudg12 <timurka.istanbul@gmail.com>
2026-04-26 21:40:10 +0300

    cat

---
+++ build/CMakeFiles/main.dir/link.d
@@ -0,0 +1,166 @@
+main: \
+  /nix/store/pdwlyjkmc6icc0wb5gaw08m9ynqrg683-glibc-2.40-218/lib/Scrt1.o \
+  /nix/store/pdwlyjkmc6icc0wb5gaw08m9ynqrg683-glibc-2.40-218/lib/crti.o \
+  /nix/store/06409a3d2d3b1113zdk9rm5a3ns4rizz-gcc-14.3.0/lib/gcc/x86_64-unknown-linux-gnu/14.3.0/crtbeginS.o \
+  CMakeFiles/main.dir/src/main.cpp.o \
+  CMakeFiles/main.dir/src/musique.cpp.o \
+  CMakeFiles/main.dir/src/utils.cpp.o \
+  /nix/store/4k2jvaryhgfir2hs9rvwfym7557p72pn-raylib-5.5-unstable-2026-01-20/lib/libraylib.so.5.5.0 \
+  /nix/store/1xw5xccqqh1xw3mvd70hyil6x418wxcm-gcc-14.3.0-lib/lib/libstdc++.so \
+  /nix/store/pdwlyjkmc6icc0wb5gaw08m9ynqrg683-glibc-2.40-218/lib/libm.so \
+  /nix/store/pdwlyjkmc6icc0wb5gaw08m9ynqrg683-glibc-2.40-218/lib/libm.so \
+  /nix/store/pdwlyjkmc6icc0wb5gaw08m9ynqrg683-glibc-2.40-218/lib/libm.so \
+  /nix/store/pdwlyjkmc6icc0wb5gaw08m9ynqrg683-glibc-2.40-218/lib/libm.so.6 \
+  /nix/store/pdwlyjkmc6icc0wb5gaw08m9ynqrg683-glibc-2.40-218/lib/libmvec.so.1 \
+  /nix/store/1xw5xccqqh1xw3mvd70hyil6x418wxcm-gcc-14.3.0-lib/lib/libgcc_s.so \
+  /nix/store/1xw5xccqqh1xw3mvd70hyil6x418wxcm-gcc-14.3.0-lib/lib/libgcc_s.so \
+  /nix/store/1xw5xccqqh1xw3mvd70hyil6x418wxcm-gcc-14.3.0-lib/lib/libgcc_s.so \
+  /nix/store/1xw5xccqqh1xw3mvd70hyil6x418wxcm-gcc-14.3.0-lib/lib/libgcc_s.so.1 \
+  /nix/store/06409a3d2d3b1113zdk9rm5a3ns4rizz-gcc-14.3.0/lib/gcc/x86_64-unknown-linux-gnu/14.3.0/libgcc.a \
+  /nix/store/06409a3d2d3b1113zdk9rm5a3ns4rizz-gcc-14.3.0/lib/gcc/x86_64-unknown-linux-gnu/14.3.0/libgcc.a \
+  /nix/store/pdwlyjkmc6icc0wb5gaw08m9ynqrg683-glibc-2.40-218/lib/libc.so \
+  /nix/store/pdwlyjkmc6icc0wb5gaw08m9ynqrg683-glibc-2.40-218/lib/libc.so \
+  /nix/store/pdwlyjkmc6icc0wb5gaw08m9ynqrg683-glibc-2.40-218/lib/libc.so \
+  /nix/store/pdwlyjkmc6icc0wb5gaw08m9ynqrg683-glibc-2.40-218/lib/libc.so.6 \
+  /nix/store/pdwlyjkmc6icc0wb5gaw08m9ynqrg683-glibc-2.40-218/lib/libc_nonshared.a \
+  /nix/store/pdwlyjkmc6icc0wb5gaw08m9ynqrg683-glibc-2.40-218/lib/ld-linux-x86-64.so.2 \
+  /nix/store/1xw5xccqqh1xw3mvd70hyil6x418wxcm-gcc-14.3.0-lib/lib/libgcc_s.so \
+  /nix/store/1xw5xccqqh1xw3mvd70hyil6x418wxcm-gcc-14.3.0-lib/lib/libgcc_s.so \
+  /nix/store/1xw5xccqqh1xw3mvd70hyil6x418wxcm-gcc-14.3.0-lib/lib/libgcc_s.so \
+  /nix/store/1xw5xccqqh1xw3mvd70hyil6x418wxcm-gcc-14.3.0-lib/lib/libgcc_s.so.1 \
+  /nix/store/06409a3d2d3b1113zdk9rm5a3ns4rizz-gcc-14.3.0/lib/gcc/x86_64-unknown-linux-gnu/14.3.0/libgcc.a \
+  /nix/store/06409a3d2d3b1113zdk9rm5a3ns4rizz-gcc-14.3.0/lib/gcc/x86_64-unknown-linux-gnu/14.3.0/libgcc.a \
+  /nix/store/06409a3d2d3b1113zdk9rm5a3ns4rizz-gcc-14.3.0/lib/gcc/x86_64-unknown-linux-gnu/14.3.0/crtendS.o \
+  /nix/store/pdwlyjkmc6icc0wb5gaw08m9ynqrg683-glibc-2.40-218/lib/crtn.o \
+  /nix/store/pdwlyjkmc6icc0wb5gaw08m9ynqrg683-glibc-2.40-218/lib/libpthread.so.0 \
+  /nix/store/39fgb5pr1r6k3szvfhpfmv9jp829kisx-libglvnd-1.7.0/lib/libOpenGL.so.0 \
+  /nix/store/39fgb5pr1r6k3szvfhpfmv9jp829kisx-libglvnd-1.7.0/lib/libGLX.so.0 \
+  /nix/store/196psj4d709f57vv7l323gdk426qf3lh-glu-9.0.3/lib/libGLU.so.1 \
+  /nix/store/xwmgapif2pzphxqdd6kshb4dy13rbqvw-glfw-3.4/lib/libglfw.so.3 \
+  /nix/store/pdwlyjkmc6icc0wb5gaw08m9ynqrg683-glibc-2.40-218/lib/ld-linux-x86-64.so.2 \
+  /nix/store/39fgb5pr1r6k3szvfhpfmv9jp829kisx-libglvnd-1.7.0/lib/libGLdispatch.so.0 \
+  /nix/store/6an80jglfqg4gkiyl87wxwa2zcplwn2f-libx11-1.8.12/lib/libX11.so.6 \
+  /nix/store/kx3mgcnyhhaz309x81gr4hjwgxn64dg8-libxext-1.3.6/lib/libXext.so.6 \
+  /nix/store/pdwlyjkmc6icc0wb5gaw08m9ynqrg683-glibc-2.40-218/lib/librt.so.1 \
+  /nix/store/pdwlyjkmc6icc0wb5gaw08m9ynqrg683-glibc-2.40-218/lib/libdl.so.2 \
+  /nix/store/3rp0mdwcvasss9r00m76h0ndlx1qx31r-libxrandr-1.5.4/lib/libXrandr.so.2 \
+  /nix/store/b5pwgqdkhmqg6lyxr6yxrkrpg9wxfisj-libxinerama-1.1.5/lib/libXinerama.so.1 \
+  /nix/store/6an80jglfqg4gkiyl87wxwa2zcplwn2f-libx11-1.8.12/lib/libX11-xcb.so.1 \
+  /nix/store/kz385xfk839k9ni7wiagn5wzhm98bhn7-libxrender-0.9.12/lib/libXrender.so.1 \
+  /nix/store/w3ikrnb4wybk6sgswn3xxl609b9mpfgj-libxcursor-1.2.3/lib/libXcursor.so.1 \
+  /nix/store/hb4d76wbkc6jiks8dj7p3fb52kgdisag-libxi-1.8.2/lib/libXi.so.6 \
+  /nix/store/qpmnp72k1p2jgxgi4f4pxkycv6fqdizk-libxxf86vm-1.1.6/lib/libXxf86vm.so.1 \
+  /nix/store/3y1g6rqwx7xjg0pb7zp7dvvssr4n5crc-libxcb-1.17.0/lib/libxcb.so.1 \
+  /nix/store/wy4crr1znd5bq0xa41r0xd76zak606yp-libxfixes-6.0.2/lib/libXfixes.so.3 \
+  /nix/store/gaw6idr0mvcr4nbjjdmlla9qyn3yxy0b-libxau-1.0.12/lib/libXau.so.6 \
+  /nix/store/jkfc5pgvhfl661y2hvn71qpaxjcb7p70-libxdmcp-1.1.5/lib/libXdmcp.so.6
+
+/nix/store/pdwlyjkmc6icc0wb5gaw08m9ynqrg683-glibc-2.40-218/lib/Scrt1.o:
+
+/nix/store/pdwlyjkmc6icc0wb5gaw08m9ynqrg683-glibc-2.40-218/lib/crti.o:
+
+/nix/store/06409a3d2d3b1113zdk9rm5a3ns4rizz-gcc-14.3.0/lib/gcc/x86_64-unknown-linux-gnu/14.3.0/crtbeginS.o:
+
+CMakeFiles/main.dir/src/main.cpp.o:
+
+CMakeFiles/main.dir/src/musique.cpp.o:
+
+CMakeFiles/main.dir/src/utils.cpp.o:
+
+/nix/store/4k2jvaryhgfir2hs9rvwfym7557p72pn-raylib-5.5-unstable-2026-01-20/lib/libraylib.so.5.5.0:
+
+/nix/store/1xw5xccqqh1xw3mvd70hyil6x418wxcm-gcc-14.3.0-lib/lib/libstdc++.so:
+
+/nix/store/pdwlyjkmc6icc0wb5gaw08m9ynqrg683-glibc-2.40-218/lib/libm.so:
+
+/nix/store/pdwlyjkmc6icc0wb5gaw08m9ynqrg683-glibc-2.40-218/lib/libm.so:
+
+/nix/store/pdwlyjkmc6icc0wb5gaw08m9ynqrg683-glibc-2.40-218/lib/libm.so:
+
+/nix/store/pdwlyjkmc6icc0wb5gaw08m9ynqrg683-glibc-2.40-218/lib/libm.so.6:
+
+/nix/store/pdwlyjkmc6icc0wb5gaw08m9ynqrg683-glibc-2.40-218/lib/libmvec.so.1:
+
+/nix/store/1xw5xccqqh1xw3mvd70hyil6x418wxcm-gcc-14.3.0-lib/lib/libgcc_s.so:
+
+/nix/store/1xw5xccqqh1xw3mvd70hyil6x418wxcm-gcc-14.3.0-lib/lib/libgcc_s.so:
+
+/nix/store/1xw5xccqqh1xw3mvd70hyil6x418wxcm-gcc-14.3.0-lib/lib/libgcc_s.so:
+
+/nix/store/1xw5xccqqh1xw3mvd70hyil6x418wxcm-gcc-14.3.0-lib/lib/libgcc_s.so.1:
+
+/nix/store/06409a3d2d3b1113zdk9rm5a3ns4rizz-gcc-14.3.0/lib/gcc/x86_64-unknown-linux-gnu/14.3.0/libgcc.a:
+
+/nix/store/06409a3d2d3b1113zdk9rm5a3ns4rizz-gcc-14.3.0/lib/gcc/x86_64-unknown-linux-gnu/14.3.0/libgcc.a:
+
+/nix/store/pdwlyjkmc6icc0wb5gaw08m9ynqrg683-glibc-2.40-218/lib/libc.so:
+
+/nix/store/pdwlyjkmc6icc0wb5gaw08m9ynqrg683-glibc-2.40-218/lib/libc.so:
+
+/nix/store/pdwlyjkmc6icc0wb5gaw08m9ynqrg683-glibc-2.40-218/lib/libc.so:
+
+/nix/store/pdwlyjkmc6icc0wb5gaw08m9ynqrg683-glibc-2.40-218/lib/libc.so.6:
+
+/nix/store/pdwlyjkmc6icc0wb5gaw08m9ynqrg683-glibc-2.40-218/lib/libc_nonshared.a:
+
+/nix/store/pdwlyjkmc6icc0wb5gaw08m9ynqrg683-glibc-2.40-218/lib/ld-linux-x86-64.so.2:
+
+/nix/store/1xw5xccqqh1xw3mvd70hyil6x418wxcm-gcc-14.3.0-lib/lib/libgcc_s.so:
+
+/nix/store/1xw5xccqqh1xw3mvd70hyil6x418wxcm-gcc-14.3.0-lib/lib/libgcc_s.so:
+
+/nix/store/1xw5xccqqh1xw3mvd70hyil6x418wxcm-gcc-14.3.0-lib/lib/libgcc_s.so:
+
+/nix/store/1xw5xccqqh1xw3mvd70hyil6x418wxcm-gcc-14.3.0-lib/lib/libgcc_s.so.1:
+
+/nix/store/06409a3d2d3b1113zdk9rm5a3ns4rizz-gcc-14.3.0/lib/gcc/x86_64-unknown-linux-gnu/14.3.0/libgcc.a:
+
+/nix/store/06409a3d2d3b1113zdk9rm5a3ns4rizz-gcc-14.3.0/lib/gcc/x86_64-unknown-linux-gnu/14.3.0/libgcc.a:
+
+/nix/store/06409a3d2d3b1113zdk9rm5a3ns4rizz-gcc-14.3.0/lib/gcc/x86_64-unknown-linux-gnu/14.3.0/crtendS.o:
+
+/nix/store/pdwlyjkmc6icc0wb5gaw08m9ynqrg683-glibc-2.40-218/lib/crtn.o:
+
+/nix/store/pdwlyjkmc6icc0wb5gaw08m9ynqrg683-glibc-2.40-218/lib/libpthread.so.0:
+
+/nix/store/39fgb5pr1r6k3szvfhpfmv9jp829kisx-libglvnd-1.7.0/lib/libOpenGL.so.0:
+
+/nix/store/39fgb5pr1r6k3szvfhpfmv9jp829kisx-libglvnd-1.7.0/lib/libGLX.so.0:
+
+/nix/store/196psj4d709f57vv7l323gdk426qf3lh-glu-9.0.3/lib/libGLU.so.1:
+
+/nix/store/xwmgapif2pzphxqdd6kshb4dy13rbqvw-glfw-3.4/lib/libglfw.so.3:
+
+/nix/store/pdwlyjkmc6icc0wb5gaw08m9ynqrg683-glibc-2.40-218/lib/ld-linux-x86-64.so.2:
+
+/nix/store/39fgb5pr1r6k3szvfhpfmv9jp829kisx-libglvnd-1.7.0/lib/libGLdispatch.so.0:
+
+/nix/store/6an80jglfqg4gkiyl87wxwa2zcplwn2f-libx11-1.8.12/lib/libX11.so.6:
+
+/nix/store/kx3mgcnyhhaz309x81gr4hjwgxn64dg8-libxext-1.3.6/lib/libXext.so.6:
+
+/nix/store/pdwlyjkmc6icc0wb5gaw08m9ynqrg683-glibc-2.40-218/lib/librt.so.1:
+
+/nix/store/pdwlyjkmc6icc0wb5gaw08m9ynqrg683-glibc-2.40-218/lib/libdl.so.2:
+
+/nix/store/3rp0mdwcvasss9r00m76h0ndlx1qx31r-libxrandr-1.5.4/lib/libXrandr.so.2:
+
+/nix/store/b5pwgqdkhmqg6lyxr6yxrkrpg9wxfisj-libxinerama-1.1.5/lib/libXinerama.so.1:
+
+/nix/store/6an80jglfqg4gkiyl87wxwa2zcplwn2f-libx11-1.8.12/lib/libX11-xcb.so.1:
+
+/nix/store/kz385xfk839k9ni7wiagn5wzhm98bhn7-libxrender-0.9.12/lib/libXrender.so.1:
+
+/nix/store/w3ikrnb4wybk6sgswn3xxl609b9mpfgj-libxcursor-1.2.3/lib/libXcursor.so.1:
+
+/nix/store/hb4d76wbkc6jiks8dj7p3fb52kgdisag-libxi-1.8.2/lib/libXi.so.6:
+
+/nix/store/qpmnp72k1p2jgxgi4f4pxkycv6fqdizk-libxxf86vm-1.1.6/lib/libXxf86vm.so.1:
+
+/nix/store/3y1g6rqwx7xjg0pb7zp7dvvssr4n5crc-libxcb-1.17.0/lib/libxcb.so.1:
+
+/nix/store/wy4crr1znd5bq0xa41r0xd76zak606yp-libxfixes-6.0.2/lib/libXfixes.so.3:
+
+/nix/store/gaw6idr0mvcr4nbjjdmlla9qyn3yxy0b-libxau-1.0.12/lib/libXau.so.6:
+
+/nix/store/jkfc5pgvhfl661y2hvn71qpaxjcb7p70-libxdmcp-1.1.5/lib/libXdmcp.so.6:
